452980f0d753716080ffb169cdf6b09270313e5e
charllab <developer.charl@gmail.com>
2020-07-21 15:28:41 -0700

    contact page updatess

--- src/themes/stay-revy/page-contact.php
+++ src/themes/stay-revy/page-contact.php
@@ -7,71 +7,40 @@
 get_header(); ?>
 
     <main>
-
         <div class="py-3">
             <div class="container">
                 <div class="row justify-content-between">
-                    <div class="col-lg-6">
+
 
                         <?php if (have_posts()) : ?>
-
                             <?php /* Start the Loop */ ?>
-
                             <?php while (have_posts()) : the_post(); ?>
-
-                                <h2 class="text-capitalize"><?php the_title(); ?></h2>
-
+                    <div class="col-12">
+                                <h2><?php the_title(); ?></h2>
+                    </div>
+                    <div class="col-lg-7">
                                 <?php the_content(); ?>
-
+                    </div>
                             <?php endwhile; ?>
-
                         <?php endif; ?>
 
-                    </div><!-- col -->
 
-                    <div class="col-lg-5">
-                        <div class="pt-3 pb-2 px-2 bg-light">
-                            <h2>Contact Information</h2>
-                            <?php
-                            $removethese = array("(", " ", ")", "-");
-                            ?>
-                            <table>
-                                <tr>
-                                    <td><strong>Phone: </strong></td>
-                                    <td>
-                                        <a href="tel:+1<?php echo strip_tel(get_field('primary_number', 'option')); ?>"><?php echo get_field('primary_number', 'option'); ?></a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="pr-1"><strong>Toll Free: </strong></td>
-                                    <td>
-                                        <a href="tel:+1<?php echo strip_tel(get_field('secondary_number', 'option')); ?>"><?php echo get_field('secondary_number', 'option'); ?></a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td><strong>E-mail: </strong></td>
-                                    <td>
-                                        <a href="mailto:<?php echo get_field('primary_email', 'option'); ?>"><?php echo get_field('primary_email', 'option'); ?></a>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td><strong>Address: </strong></td>
-                                    <td><?php echo get_field('physical_address', 'option'); ?></td>
-                                </tr>
-                            </table>
-                        </div><!-- bg-light -->
-
-                        <div class="px-0">
-                            <?php
-                            echo get_field('map_embed_code', 'option');
-                            ?>
-                        </div><!-- px-0 -->
-                    </div><!-- col -->
-                </div><!-- row -->
-
-            </div><!-- container -->
+                    <div class="col-lg-4">
+                        <div class="px-2 py-1">
+                            <p class="text-body">
+                                <i class="fas fa-phone-alt mr-35"></i> <strong>Tel</strong>: <a href="tel:+1<?php echo strip_tel(get_field('phone_number', 'options')); ?>" class="text-body"><?php the_field('phone_number', 'options'); ?></a>
+                            </p>
+                            <p class="text-body">
+                                <i class="fas fa-envelope mr-35"></i> <strong>Email</strong>: <a href="mailto:<?php the_field('email_address', 'options'); ?>" class="text-body"><?php the_field('email_address', 'options'); ?></a>
+                            </p>
+                            <p class="text-body mb-2">
+                                <i class="fas fa-map-marker-alt align-top pt-250 mr-35"></i> <span class="d-inline-block"> <strong>Address</strong>: <?php echo the_field('postal_address', 'options')?></span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
     </main>
 
 <?php get_footer();
