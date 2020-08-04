0497bd15bb58f1c4cc9c25f32cea647419eba9f7
charllab <developer.charl@gmail.com>
2020-08-04 13:49:04 -0700

    added analytics

--- src/themes/stay-revy/header.php
+++ src/themes/stay-revy/header.php
@@ -13,6 +13,31 @@
     <script src="https://kit.fontawesome.com/385662c85c.js" crossorigin="anonymous" SameSite="none Secure"></script>
 
     <?php wp_head(); ?>
+
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-4700177-78"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-4700177-78');
+    </script>
+
+    <!-- Hotjar Tracking Code for https://stayrevy.com -->
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:1931617,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
+
+
 </head>
 
 <body <?php body_class(); ?>>
